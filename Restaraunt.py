e5fcd28a1b47aca6387b7d5b071fa4571542dc88
Kaoru <topgearchesak@gmail.com>
2023-05-16 20:43:12 +0300

    extend def number_served
    + def update_number_served

--- Restaraunt.py
+++ Restaraunt.py
@@ -6,6 +6,7 @@ class Restaraunt():
         self.restaraunt_name = restaraunt_name
         self.cuisine_type = cuisine_type
         self.location = location
+        self.quantity_res = 0
     
     
     def describe_restaraunt(self):
@@ -24,6 +25,13 @@ class Restaraunt():
     
     def open_restaraunt(self):
         print(f'\n--- {self.restaraunt_name.upper()} открыт ---')
+    
+    def number_served(self):
+        print(self.quantity_res) 
+    
+    def update_number_served(self, quantity):
+        self.quantity_res += quantity
+
 
 res_1 = Restaraunt('homeworks', 'совмещенная')
 res_1.open_restaraunt()
@@ -32,3 +40,5 @@ res_1.describe_restaraunt()
 res_2 = Restaraunt('бражник', 'крутая', 'ул. Ленина, 18, Санкт-Петербург, 197198')
 res_2.open_restaraunt()
 res_2.describe_restaraunt()
+res_2.update_number_served(24)
+res_2.number_served()
