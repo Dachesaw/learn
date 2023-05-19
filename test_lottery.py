abe34487347ccddfd0555dd31f57168a8e4d0d19
Kaoru <topgearchesak@gmail.com>
2023-05-19 14:04:50 +0300

    поиск билета

---
+++ test_lottery.py
@@ -0,0 +1,15 @@
+from random import choice
+
+bilet_nomer = ('a', 'b', 'c', 'd', 'd', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+
+def bilet(*args):
+    bilet_nomer = str(choice(args))
+    return bilet_nomer
+k = 0
+while True:
+    k += 1
+    full_bilet = bilet(*bilet_nomer) + bilet(*bilet_nomer) + bilet(*bilet_nomer) + bilet(*bilet_nomer)
+    if full_bilet == 'bad2':
+        print(f'Выйгрышный билет: {full_bilet}\nПопытка: {k}')
+        break
